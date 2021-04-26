732dd5da11c89cac6079f0620b6791aa9b1d4699
Tbird-ops <twstapert@gmail.com>
2021-04-26 12:36:37 -0700

    Kicking off the catan main!

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("Hello World!")
+import random as rand
+
+### Setup ###
+
+num_players = None
+players_cards = None
+
+
+### Functions ###
+
+def gen_map():
+  # lay tiles
+  # number the tiles
+  # port locations
+  pass
+
+def Dice():
+  d1 = rand.randint(1,6)
+  d2 = rand.randint(1,6)
+  roll = d1 + d2
+  return roll
+
+def trade():
+  # original 4:1
+  # port specific 3:1 or special 2:1
+  # with another player
+  pass
+
+def build():
+  # purchase something
+  # place or upgrade
+  pass
+
+
+
+
+### Initialize ###
