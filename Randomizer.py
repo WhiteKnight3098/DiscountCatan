5a372e6e518d1765b1670f43cb6aad671a4eed0c
Tbird-ops <twstapert@gmail.com>
2021-04-29 12:32:18 -0700

    Added tiles to the Tokenlogger list

--- Randomizer.py
+++ Randomizer.py
@@ -1,7 +1,5 @@
 import random as rand
 
-
-
 def DiceRoll():
   alpha = rand.randint(1,6)
   beta = rand.randint(1,6)
@@ -10,10 +8,9 @@ def DiceRoll():
 
 def TokenLogger():
   randomized_tokens = []
-  tiles = []
+  tiles = ["Sheep","Sheep","Sheep","Sheep", "Wood","Wood","Wood","Wood","Crop","Crop","Crop","Crop","Rock","Rock","Rock","Clay","Clay",]
   values_list = [2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12]
   for i in values_list:
     current = 19 - i
     number = rand.randint(0,current)
     randomized_tokens.append(number)
-  return randomized_tokens
