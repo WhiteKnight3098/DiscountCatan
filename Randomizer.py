a5c60af506342c4ae7a8025f3a7b08aadd8428b4
Tbird-ops <twstapert@gmail.com>
2021-04-29 12:22:22 -0700

    TokenLogger has been added to Randomizer for the use of generating tile values

--- Randomizer.py
+++ Randomizer.py
@@ -1,7 +1,18 @@
 import random as rand
 
+
+
 def DiceRoll():
-    alpha = rand.randint(1,6)
-    beta = rand.randint(1,6)
-    fina = alpha + beta
-    return fina
+  alpha = rand.randint(1,6)
+  beta = rand.randint(1,6)
+  fina = alpha + beta
+  return fina
+
+def TokenLogger():
+  randomized_tokens = []
+  tiles = []
+  values_list = [2, 3, 3, 4, 4, 5, 5,  6, 6, 7, 7,  8, 8, 9,  9, 10, 10, 11, 11, 12]
+  for i in values_list:
+    current = 19 - i
+    number = rand.randint(0,current)
+    randomized_tokens.append(number)
