d24b6001858548602bc9136eb5d927c02a23c304
Tbird-ops <twstapert@gmail.com>
2021-04-29 12:43:51 -0700

    minor edit to Tokenlogger

--- Randomizer.py
+++ Randomizer.py
@@ -6,10 +6,11 @@ def DiceRoll():
   fina = alpha + beta
   return fina
 
+
+# Desert tile is not included, neither are 7s which trigger the Bandit
 def TokenLogger():
   randomized_tokens = []
-  tiles = ["Sheep","Sheep","Sheep","Sheep", "Wood","Wood","Wood","Wood","Crop","Crop","Crop","Crop","Rock","Rock","Rock","Clay","Clay",]
-  values_list = [2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12]
+  values_list = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12]
   for i in values_list:
     current = 19 - i
     number = rand.randint(0,current)
