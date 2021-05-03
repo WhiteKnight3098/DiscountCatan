9bd52d3f0dc37635f663a1a13552c29e2a8ffa4f
Tbird-ops <twstapert@gmail.com>
2021-05-03 12:48:24 -0700

    Return Resource values function

--- Player.py
+++ Player.py
@@ -3,22 +3,34 @@ class Player():
     self.name = name
     self.player_hand = []  #dev cards
     self.resources = []  #Resource only
+    self.stone = []
+    self.clay = []
+    self.wheat = []
+    self.sheep = []
+    self.wood = []
 
   # Resource Collection and Trades
   def addResource(self, diceroll, random_values, tiles):
-
     indexValues = []
-
     for number in range(len(random_values)):
       if diceroll == random_values[number]:
         indexValues.append(number)
-    
     self.resources.append(tiles[number])
-      
     pass
-  def returnstone(self):
-    value = self.resources[0]
-    return value
+
+  def returnResources(self):
+    for index in range(len(self.resources)):
+      if self.resources[index] == "Stone":
+        self.stone.append(index)
+      elif self.resources[index] == "Clay":
+        self.clay.append(index)
+      elif self.resources[index] == "Wheat":
+        self.wheat.append(index)
+      elif self.resources[index] == "Sheep":
+        self.sheep.append(index)
+      elif self.resources[index] == "Wood":
+        self.wood.append(index)
+
   def spendResource():
     pass
 
