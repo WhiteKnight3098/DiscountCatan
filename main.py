dbc7fac8352665edc055208ebdc7392cb1c23901
Tbird-ops <twstapert@gmail.com>
2021-05-08 21:07:04 -0700

    Moving game components to TKinter interface working on more functionality Co-authored-by: bgpilot16@gmail.com <bgpilot16@gmail.com>

--- main.py
+++ main.py
@@ -1,56 +1,35 @@
 import random as rand
-import time
-import tkinter as tk
+from time import sleep
+from tkinter import *
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 ###SETUP###
+root = Tk()
+root.geometry('400x400')
+
+
 
 #player
 Coffee_Made = 0
 Level = 0
 Money = 100
 sales_price = 1.00
-Baristas = 0
-Managers = 0
 
-Beans = 0
-Bean_price = []
+Baristas = 0 #improves user clicks
+Employee_Cost = 10
+Shops = 0 #controls autoclick production
+Expansion_Cost = 200
+
+Beans = 50
+Bean_prices = []
 Beans_rate = 200
 
 ###FUNCTIONS###
 
-def makeCoffee():
-  global Coffee_Made
-  global Beans
-
-  if Beans >= 1:
-    Coffee_Made += 1
-    Beans -= 1
-
-def buyBeans():
-  global Money
-  global Beans
-
-  index = len(Bean_price) - 1
-  cost = Bean_price[index]
-  if Money >= cost:
-    Money = Money - cost
-    Beans += Beans_rate
-
-def bean_price_relay():
-  price = rand.randint(20,50)
-  Bean_price.append(price)
-"""   current = len(Bean_price) - 1
-  print("Bean price: ${}".format(Bean_price[current])) """
-
-def bean_plot():
-  pass
-
-while True:
+def update_leaderboard():
   bean_price_relay()
-
   print('''
   Current Status!
   Coffee Made: {}
@@ -58,15 +37,94 @@ while True:
   Sale Price: {}
   Beans: {}
   Bean Price: {}
-  '''.format(Coffee_Made,Money,sales_price,Beans,Bean_price))
+  Baristas: {}
+  Employee_Cost: {}
+  Shops: {}
+  Expansion Cost: {}
+  '''.format(Coffee_Made,Money,sales_price,Beans,Bean_prices,Baristas, Employee_Cost, Shops, Expansion_Cost,)) 
 
-  print("Would you like to make coffee or buy beans?")
-  choice = int(input("1. Coffee\n2. Beans\n"))
-  if choice == 1:
-    makeCoffee()
-  elif choice == 2:
-    buyBeans()
+def makeCoffee():
+  global Coffee_Made
+  global Beans
+  global Money
+
+  if Baristas >= 1:
+    if Beans >= 1:
+      Coffee_Made += 1
+      Beans -= 1
+      Money += sales_price
+    else:
+      print("Out of beans!")
+    for b in range(Baristas):
+      if Beans >= 1:
+        Coffee_Made += 1
+        Beans -= 1
+        Money += sales_price
+      else:
+        ("You ran out of beans!")
+  elif Beans >= 1:
+    Coffee_Made += 1
+    Beans -= 1
+    Money += sales_price
   else:
-    print("Sorry! No can do!")
-  #So that the money doesn't run out
-  Money = 100
+    print("No beans available for use.")
+  print(Coffee_Made)
+  update_leaderboard()
+
+#Put this here because bad organization. Worse than my cable management lmao    
+btn = Button(root, text = "Make Coffee", bd = 5, command = makeCoffee)
+btn.pack(side = 'top')
+
+
+def buyBeans():
+  global Money
+  global Beans
+
+  index = len(Bean_prices) - 1
+  cost = Bean_prices[index]
+  if Money >= cost:
+    Money = Money - cost
+    Beans += Beans_rate
+  else:
+    print("Lacking sufficient funds to purchase beans.")
+  update_leaderboard()
+
+def bean_price_relay():
+  price = rand.randint(20,50)
+  Bean_prices.append(price)
+"""   current = len(Bean_prices) - 1
+  print("Bean price: ${}".format(Bean_prices[current])) """
+
+def hire_barista():
+  global Money
+  global Baristas
+  global Employee_Cost
+
+  if Money >= Employee_Cost:
+    Money -= Employee_Cost
+    Baristas += 1
+    Employee_Cost = Employee_Cost + (Employee_Cost/2)
+  else:
+    print("Lacking sufficient funds to hire staff.")
+  update_leaderboard()
+
+#TODO: Make Sale price function for raising and lowering coffee cost
+def sales_price_up():
+  global sales_price
+  sales_price += 0.05
+def sales_price_down():
+  global sales_price
+  sales_price -= 0.05
+
+#TODO: make the purchase for an Autoclicker.
+def purchase_shop():
+  pass
+
+#TODO: make matplotlib chart of current and past bean prices.
+def bean_plot():
+  pass
+
+
+#TODO: Eventually I think the while loop needs to become a TKinter mainloop. Thus allowing the .after() method for the autoclicker functions.
+#       for testing, this is just jerry-rigged thing to get us working.
+root.mainloop()
