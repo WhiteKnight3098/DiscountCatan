bba8ba2ee3d9cce2ef32e26edb60f770f9488ce3
Tbird-ops <twstapert@gmail.com>
2021-05-13 12:54:54 -0700

    Cleaning up and Commenting

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+#Credits:
+#Tristan Stapert did a vast majority of the backend stuff, while Ben Gress focused on GUI and print outs within the window.
+
 import random as rand
 from tkinter import *
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-#Credits:
-#Tristan Stapert did a vast majority of the backend stuff, while Ben Gress focused on GUI and printing to the news feed.
+### SETUP ###
 
-###SETUP###
+## Major Window components (Not yet including the graph) ##
+# Root holds the TKinter widgets
 root = Tk()
 root.geometry('800x400')
+
+#Current Statistics Text Box Widget
 leaderboard = Text(root, width = 35, height = 15, wrap='none')
 leaderboard.grid(column=1, columnspan=1, row=1, rowspan= 5)
+
+#Game Updates Widget
 newsfeed = Text(root, width = 100, height = 5, wrap='none')
 newsfeed.grid(column=1, columnspan=20, row=6, rowspan= 1)
 
-#TODO: Move function print statements into text box newsfeed
-
-#player
+## Player Stats ##
+# Generic Player Stats. Will be updated as game is played.
 Coffee_Made = 0
 Level = 0
 Money = 100.00
@@ -24,16 +30,26 @@ sales_price = 3.00
 
 Baristas = 0 #improves user clicks
 Employee_Cost = 10.00
+
 Shops = 0 #controls autoclick production
 Expansion_Cost = 200.00
 
+## Bean Controls ##
+#starting supply
 Beans = 50
+
+# current and past minute prices for the graph
 Bean_prices = []
 Bean_Current = 0
+
+# Purchase rate
 Beans_rate = 200
 
-###FUNCTIONS###
 
+
+### FUNCTIONS ###
+
+# Update the current game statistics
 def update_leaderboard():
   leaderboard.delete("1.0", "end")
   leaderboard.insert("end", 
@@ -50,12 +66,15 @@ def update_leaderboard():
   Expansion Cost: {}
   '''.format(Coffee_Made,Money,sales_price,Beans,Bean_Current,Baristas,Employee_Cost,Shops,Expansion_Cost))
 
+# Inputs the news feed for the failed purchases
 def insufficientFunds():
   newsfeed.insert("1.0", "Lack of sufficient funds!\n")
 
+# Inputs the news feed for the failed coffee production
 def noBeans():
   newsfeed.insert("1.0", "No more beans!\n")
 
+# Main function of the game. Makes a coffee and adds income to the current Money
 def makeCoffee():
   global Coffee_Made
   global Beans
@@ -83,11 +102,12 @@ def makeCoffee():
     noBeans()
   update_leaderboard()
 
-#Put this here because bad organization. Worse than my cable management lmao    
+# Button layout for the buttons of the game
 Coffeebtn = Button(root, text = "Make Coffee", bd = 2, command = makeCoffee)
 Coffeebtn.grid(column=2, row=1)
 
 
+# Adds more beans to the current stock
 def buyBeans():
   global Money
   global Beans
@@ -106,7 +126,7 @@ Beansbtn = Button(root, text = "Buy Beans", bd = 2, command= buyBeans)
 Beansbtn.grid(column=2,row=2)
 
 
-#TODO: Make Button
+# Improve output rate of manual production
 def hire_barista():
   global Money
   global Baristas
@@ -120,12 +140,12 @@ def hire_barista():
   else:
     insufficientFunds()
   update_leaderboard()
+
 BaristaButton = Button(root, text = "Hire Barista", bd = 2, command = hire_barista)
 BaristaButton.grid(column=2, row=3)
 
 
-#TODO: make the purchase for an Autoclicker.
-#TODO: Make button on GUI
+# Purchase a shop expansion to start generating income
 def purchase_shop():
   global Money
   global Shops
@@ -143,7 +163,8 @@ def purchase_shop():
 ShopButton = Button(root, text = "Expand Shop", bd = 2, command= purchase_shop)
 ShopButton.grid(column=2, row=4)
 
-#Auto clicker thing
+
+#Auto clicker function for Shop Expansion
 def shop_generator():
   global Money
   global Coffee_Made
@@ -157,10 +178,9 @@ def shop_generator():
   root.after(1000, shop_generator)
 
 
-#TODO: make matplotlib chart of current and past bean prices.
+# Graph control for the Bean Sale Prices for the last minute
 def bean_plot(i):
   xtime = []
-
   if len(Bean_prices) < 16:
     for x in range(len(Bean_prices)):
       xtime.append(x)
@@ -171,10 +191,11 @@ def bean_plot(i):
   chart.plot(xtime, Bean_prices)
 
 
-
+#Function that automatically generates the next bean prices that will be used.
 # NO BUTTON NEEDED
 def bean_price_relay():
   global Bean_Current
+
   list_length =  len(Bean_prices)
   if list_length > 14:
     Bean_prices.pop(0)
@@ -186,7 +207,7 @@ def bean_price_relay():
   root.after(5000, bean_price_relay)
 
 
-
+### Initiation ###
 
 #Start timed automation
 shop_generator()
